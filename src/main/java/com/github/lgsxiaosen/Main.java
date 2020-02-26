4798295e02ef8aa7391b73e302aa998169305ceb
liuguisen <liuguisen@blued.com>
2020-02-26 08:23:46 +0800

    first commit

---
+++ src/main/java/com/github/lgsxiaosen/Main.java
@@ -0,0 +1,12 @@
+package com.github.lgsxiaosen;
+
+/**
+ * @author sen
+ * 2020/2/26 08:19
+ */
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World !");
+    }
+}
